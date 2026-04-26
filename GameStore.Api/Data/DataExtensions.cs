6382e2a7d49c8d3db0b99e56eabc847ba97f915a
Daniel Herrera <70177673+DanEra1998@users.noreply.github.com>
2026-04-26 11:47:51 -0700

    removing hardcoded db connection string to settings file, this only work locally on local machine

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
@@ -13,5 +14,39 @@ public static class DataExtensions
         // at this point we have access to an instance 
         
         dbContext.Database.Migrate();
+    }
+
+    public static void AddGameStoreDb(this WebApplicationBuilder builder)
+    {
+        // research DEPENDENCY INJECTION 
+        var connString = builder.Configuration.GetConnectionString("GameStore"); // RECALL IT IS BAD TO HARDCODE
+//registration of db context with connString
+        builder.Services.AddSqlite<GameStoreContext>(
+            connString, 
+            optionsAction: options => options.UseSeeding((context, _) =>
+            {
+                //Data seeding is the process of pre-populating your database with initial data when it's first created.
+                //Think of it as the default data your app needs to function.
+                if (!context.Set<Genre>().Any())
+                {
+                    context.Set<Genre>().AddRange(
+                        new Genre { Name = "Fighting" },
+                        new Genre { Name = "RPG" },
+                        new Genre { Name = "Platformer" },
+                        new Genre { Name = "Racing" },
+                        new Genre { Name = "Sports" },
+                        new Genre { Name = "Shooter" },
+                        new Genre { Name = "Sandbox" },
+                        new Genre { Name = "Puzzle" },
+                        new Genre { Name = "Simulation" },
+                        new Genre { Name = "Adventure" }
+                    );
+                    //we also have to save this into the db
+                    context.SaveChanges();
+                }
+            })
+        );
+// after this we are ready to talk to sqlite db, we can take advantage entity framework migration features
+
     }
 }
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,18 +1,15 @@
 using GameStore.Api.Data;
 using GameStore.Api.DTOs;
 using GameStore.Api.Endpoints;
+using GameStore.Api.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
 // we will activate/register input validation using builder such that we can enforce [Required] field within 
 //CreateGameDTO file
 builder.Services.AddValidation();
-
-// research DEPENDENCY INJECTION 
-var connString = "Data Source=GameStore.db";
-//registration of db context with connString
-builder.Services.AddSqlite<GameStoreContext>(connString);
-// after this we are ready to talk to sqlite db, we can take advantage entity framework migration features
+//all database logic is within our own created AddGameStoreDB which we extended EF (requires static) 
+builder.AddGameStoreDb();
 
 //all validation services will be available for all our endpoints
 var app = builder.Build();
